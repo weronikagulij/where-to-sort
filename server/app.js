c33c5ccc2522f06d1615e640ffc51d33bc5e0967
Weronika Jankowska <weronika.jankowska098@gmail.com>
2020-01-08 15:08:17 +0100

    Joi comments removed

--- server/app.js
+++ server/app.js
@@ -5,9 +5,9 @@ require('dotenv/config');
 
 const app = express();
 const postsRoute = require('./routes/posts');
+const db = mongoose.connection;
 
 app.use(bodyParser.json());
-
 app.use('/posts', postsRoute);
 
 app.get('/', (req, res) => {
@@ -19,54 +19,9 @@ mongoose.connect(process.env.DB_CONNECTION, {
     useUnifiedTopology: true
 });
 
-var db = mongoose.connection;
-
 db.on('error', console.error.bind(console, 'connection error:'));
-
 db.once('open', function() {
     console.log('Connection Successful!');
-
-    // // define Schema
-    // var BookSchema = mongoose.Schema({
-    //     name: String,
-    //     price: Number,
-    //     quantity: Number
-    // });
-
-    // // compile schema to model
-    // var Book = mongoose.model('Book', BookSchema, 'bookstore');
-
-    // // a document instance
-    // var book1 = new Book({
-    //     name: 'Introduction to Mongoose',
-    //     price: 10,
-    //     quantity: 25
-    // });
-
-    // // save model to database
-    // book1.save(function(err, book) {
-    //     if (err) return console.error(err);
-    //     console.log(book.name + ' saved to bookstore collection.');
-    // });
 });
-
-// const post = mongoose.Schema({
-//     title: {
-//         type: String,
-//         required: true
-//     },
-//     date: {
-//         type: Date,
-//         default: Date.now
-//     }
-// });
-
-// post.save()
-//     .then(data => {
-//         console.log('dfd');
-//     })
-//     .catch(err => {
-//         console.log('dfddd');
-//     });
 
 app.listen(3000);
--- server/models/Post.js
+++ server/models/Post.js
@@ -4,13 +4,4 @@ const PostSchema = mongoose.Schema({
     title: String
 });
 
-// const Joi = require('joi');
-
-// const Post = {
-//     title: Joi.string()
-//         .min(3)
-//         .required()
-// };
-
 module.exports = mongoose.model('Posts', PostSchema);
-// module.exports = Post;
